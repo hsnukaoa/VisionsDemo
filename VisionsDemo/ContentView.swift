04add7179b2a8994df7f9a5e0dbf252ef316f642
koudagawa <koudagawa@icloud.com>
2025-10-31 20:23:52 +0900

    Initial Commit

---
+++ VisionsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionsDemo
+//
+//  Created by 宇田川航太 on 2025/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionsDemo/VisionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionsDemoApp.swift
+//  VisionsDemo
+//
+//  Created by 宇田川航太 on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct VisionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
